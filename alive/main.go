a1580db549433da8f760cfe9f667061e46f01d7a
Sheya Bernstein <sheyaberns@gmail.com>
2025-01-07 18:39:35 +0000

    Suppress logs in release mode

--- alive/main.go
+++ alive/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 
@@ -10,12 +11,23 @@ import (
 	"alive/alive/routes"
 )
 
-var defaultPort int = 4444
+var (
+	defaultListenHost = "::"
+	defaultListenPort = 4444
+)
 
-func getPort() int {
-	portStr := os.Getenv("PORT")
+func getListenHost() string {
+	host := os.Getenv("LISTEN_HOST")
+	if host == "" {
+		host = defaultListenHost
+	}
+	return host
+}
+
+func getListenPort() int {
+	portStr := os.Getenv("LISTEN_PORT")
 	if portStr == "" {
-		portStr = strconv.Itoa(defaultPort)
+		portStr = strconv.Itoa(defaultListenPort)
 	}
 
 	port, err := strconv.Atoi(portStr)
@@ -26,14 +38,28 @@ func getPort() int {
 	return port
 }
 
+func formatAddress(host string, port int) string {
+	if host == defaultListenHost {
+		return fmt.Sprintf("[%s]:%d", host, port)
+	}
+	return fmt.Sprintf("%s:%d", host, port)
+}
+
 func main() {
+	if gin.Mode() == gin.ReleaseMode {
+		gin.DefaultWriter = io.Discard
+	} else {
+		gin.DefaultWriter = os.Stdout
+	}
+
 	router := gin.Default()
 
 	router.GET("/healthz", routes.Health)
 	router.GET("/liveness", routes.Liveness)
 
-	port := getPort()
-	addr := fmt.Sprintf("0.0.0.0:%d", port)
+	host := getListenHost()
+	port := getListenPort()
+	addr := formatAddress(host, port)
 	fmt.Println("Listening on", addr)
 
 	err := router.Run(addr)
